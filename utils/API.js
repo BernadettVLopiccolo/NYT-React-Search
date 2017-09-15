c7029fd18bdc4ecbda7953edb5152a436c2c1319
Bernadett Villam Lopiccolo <bernadettvillam@yahoo.com>
2017-09-15 11:01:22 -0700

    added utils folder, App.js and index.js

---
+++ utils/API.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+const BASEURL = "https://www.nytimes.com/?t=";
+const APIKEY = "&apikey=3f45657060084093b45d99bb4190658c";
+
+export default {
+  search: function(query) {
+    return axios.get(BASEURL + query + APIKEY);
+  }
+};
